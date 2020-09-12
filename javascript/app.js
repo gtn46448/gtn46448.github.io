6ea17b6676d1007c9619d2eb051fa40c64f6257e
gtn46448 <54673210+gtn46448@users.noreply.github.com>
2020-09-12 04:25:26 -0500

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -18,59 +18,61 @@ const linkAnimation = () => {
   });
 }
 
+const overlayManage = () => {
+  const cardOverlay = document.querySelector('.cardOverlay');
+  const workCards = document.querySelectorAll('.workCard');
+  const body = document.body;
+  const navButtons = document.querySelector('.navButtons');
 
-const cardOverlay = document.querySelector('.cardOverlay');
-const workCards = document.querySelectorAll('.workCard');
-const body = document.body;
-const navButtons = document.querySelector('.navButtons');
+  workCards.forEach((workCard) => {
+    workCard.addEventListener("click", () => {
+      cardOverlay.classList.add("open");
+      // measure width before vert scrollbar removed
+      const w1 = document.body.clientWidth;
+      //inadvertantly removes scrollbar from body (but adds it to overlay)
+      body.classList.add("noScroll");
+      //measure width after sb removal
+      const w2 = document.body.clientWidth;
+      //add padding to avoid reflow
+      body.style.padding = `0px ${w2-w1}px 0px 0px`;
+      navButtons.style.padding =  `0px ${w2-w1}px 0px 0px`;
 
-workCards.forEach((workCard) => {
-  workCard.addEventListener("click", () => {
-    cardOverlay.classList.add("open");
-    // measure width before vert scrollbar removed
-    const w1 = document.body.clientWidth;
-    //inadvertantly removes scrollbar from body (but adds it to overlay)
-    body.classList.add("noScroll");
-    //measure width after sb removal
-    const w2 = document.body.clientWidth;
-    //add padding to avoid reflow
-    body.style.padding = `0px ${w2-w1}px 0px 0px`;
-    navButtons.style.padding =  `0px ${w2-w1}px 0px 0px`;
+      cardOverlay.setAttribute('aria-hidden', false);
+      //cardFullBox is the one that scrolls rather than cardOverlay
+      document.querySelector('.cardFullBox').scrollTop = 0;
 
-    cardOverlay.setAttribute('aria-hidden', false);
-    //cardFullBox is the one that scrolls rather than cardOverlay
-    document.querySelector('.cardFullBox').scrollTop = 0;
+      const fullImage = document.querySelector('.cardFullImage');
+      const cardImage = workCard.querySelector('.cardPreviewImage');
+      fullImage.setAttribute('src', cardImage.getAttribute('src'))
 
-    const fullImage = document.querySelector('.cardFullImage');
-    const cardImage = workCard.querySelector('.cardPreviewImage');
-    fullImage.setAttribute('src', cardImage.getAttribute('src'))
+      const fullTitle = document.querySelector('.cardFullTitle');
+      const cardTitle = workCard.querySelector('.cardTitle');
+      fullTitle.innerHTML = cardTitle.innerHTML;
 
-    const fullTitle = document.querySelector('.cardFullTitle');
-    const cardTitle = workCard.querySelector('.cardTitle');
-    fullTitle.innerHTML = cardTitle.innerHTML;
+      const fullDesc = document.querySelector('.cardFullDescription');
+      const cardDesc = workCard.getAttribute('descFull');
+      console.log(cardDesc);
+      fullDesc.innerHTML = cardDesc;
 
-    const fullDesc = document.querySelector('.cardFullDescription');
-    const cardDesc = workCard.getAttribute('descFull');
-    console.log(cardDesc);
-    fullDesc.innerHTML = cardDesc;
-
+    })
   })
-})
 
-cardOverlay.addEventListener("click", (e) => {
-  console.log(e);
-  if(e.target.classList.contains("overlayClose")) {
-    cardOverlay.classList.remove("open");
-    body.classList.remove("noScroll");
-    body.style.padding = '0';
-    navButtons.style.padding =  '0';
-    cardOverlay.setAttribute('aria-hidden', true);
-  }
-})
+  cardOverlay.addEventListener("click", (e) => {
+    console.log(e);
+    if(e.target.classList.contains("overlayClose")) {
+      cardOverlay.classList.remove("open");
+      body.classList.remove("noScroll");
+      body.style.padding = '0';
+      navButtons.style.padding =  '0';
+      cardOverlay.setAttribute('aria-hidden', true);
+    }
+  })
+}
 
 const main = () => {
   navSlide();
   linkAnimation();
+  overlayManage();
 }
 
 main();
